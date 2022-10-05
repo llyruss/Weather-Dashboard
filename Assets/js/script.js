275b68673add08bfd5ac9e41c1f2358c80f74da7
Lily Russ <llyruss@yahoo.com>
2022-10-05 15:15:30 -0500

    fetch request to turn city into lat and lon

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,29 @@ let today = moment().format("M/D/YYYY")
 
 console.log(today)
 
-let citySearched = "Minneapolis"
+let citySearched = "minneapolis"
 
-let getLatLong = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearched + "&limit={1}&appid=" + APIKey
+//get city lat and long
+let getLatLongURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearched + "&limit=1&appid=" + APIKey
+
+console.log(getLatLongURL)
+
+fetch(getLatLongURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function (latLong){
+        console.log(latLong)
+
+        let cityLat = latLong[0].lat
+        let cityLong =latLong[0].lon
+
+        console.log(cityLong)
+    })
+
+
+    //get same day  forcast information
+
+    let sameDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearched+"&appid=" + APIKey
+    console.log(sameDayURL)
+    

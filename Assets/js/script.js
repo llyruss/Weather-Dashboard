595e5393dd42b105eb305a32d6a9b9837406af2f
Lily Russ <llyruss@yahoo.com>
2022-10-06 11:48:04 -0500

    fetch request for today's weather

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,31 +1,41 @@
 let APIKey = "a855332de88e2807d74d20b390983564"
 let today = moment().format("M/D/YYYY")
 
-console.log(today)
 
 let citySearched = "minneapolis"
 
 //get city lat and long
 let getLatLongURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearched + "&limit=1&appid=" + APIKey
 
-console.log(getLatLongURL)
 
 fetch(getLatLongURL)
     .then(function(response){
         return response.json()
     })
     .then(function (latLong){
-        console.log(latLong)
 
         let cityLat = latLong[0].lat
         let cityLong =latLong[0].lon
 
-        console.log(cityLong)
     })
 
 
     //get same day  forcast information
 
-    let sameDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearched+"&appid=" + APIKey
+    let sameDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearched+"&appid=" + APIKey+"&units=imperial"
     console.log(sameDayURL)
-    
+
+    fetch(sameDayURL)
+        .then(function(response2){
+            return response2.json()
+        })
+        .then(function(dailyWeather){
+            console.log(dailyWeather)
+
+            let todayTemp = dailyWeather.main.temp
+            let todayWind = dailyWeather.wind.speed
+            let todayHumidity = dailyWeather.main.humidity
+            let todayIconCode = dailyWeather.weather[0].icon
+            let todayIcon = "http://openweathermap.org/img/w/"+todayIconCode+".png"
+
+        })

c26ea65c2edeb72d10d3001f5124efe04d1dd088
Lily Russ <llyruss@yahoo.com>
2022-10-06 14:33:52 -0500

    made current weather info display to the page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,35 +2,33 @@ let APIKey = "a855332de88e2807d74d20b390983564"
 let today = moment().format("M/D/YYYY")
 
 
-let citySearched = "minneapolis"
+let citySearched = "Minneapolis"
 
 //get city lat and long
-let getLatLongURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearched + "&limit=1&appid=" + APIKey
+// let getLatLongURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySearched + "&limit=1&appid=" + APIKey
 
 
-fetch(getLatLongURL)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function (latLong){
+// fetch(getLatLongURL)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(function (latLong){
 
-        let cityLat = latLong[0].lat
-        let cityLong =latLong[0].lon
+//         let cityLat = latLong[0].lat
+//         let cityLong =latLong[0].lon
 
-    })
+//     })
 
 
 //get same day  forcast information
 
 let sameDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearched+"&appid=" + APIKey+"&units=imperial"
-    console.log(sameDayURL)
 
  fetch(sameDayURL)
      .then(function(response2){
         return response2.json()
      })
      .then(function(dailyWeather){
-         console.log(dailyWeather)
 
         let todayTemp = dailyWeather.main.temp
         let todayWind = dailyWeather.wind.speed
@@ -38,6 +36,22 @@ let sameDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" +citySearc
         let todayIconCode = dailyWeather.weather[0].icon
         let todayIcon = "http://openweathermap.org/img/w/"+todayIconCode+".png"
 
+         //display same day forcast
+
+         let chosenCity = document.getElementById("chosenCity")
+         let currentTemp=document.getElementById("currentTemp")
+         let currentWind=document.getElementById("currentWind")
+         let currentHumidity=document.getElementById("currentHumidity")
+
+         chosenCity.textContent = citySearched +" "+today
+         currentTemp.textContent = todayTemp
+         currentWind.textContent = todayWind
+         currentHumidity.textContent = todayHumidity
+
+
         })
+
+
+
 
 //get 5 day forcast
